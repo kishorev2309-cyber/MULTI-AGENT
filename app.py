d84867be8f391137f0b097b46071cbb310e19c3c
kishorev2309-cyber <kishorev2309@gmail.com>
2026-04-16 19:10:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def analyze():
     
     return jsonify(result)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
